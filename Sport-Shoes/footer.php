729088572b08a479ac32c6367c940365a70d6742
JonathanBeckerBCK <jonathanbe.ifpr2024@gmail.com>
2025-12-12 21:26:49 -0300

    Add files via upload

---
+++ Sport-Shoes/footer.php
@@ -0,0 +1,47 @@
+<footer style="background-color: #333333; padding: 1%;">
+            <div class="footer-content">
+                <div class="lista">
+                    <h1 style="color: rgb(255, 255, 255);">Contatos</h1>
+                    <ul>
+                        <li">+55 (45) 91232-2342</li>
+                            <br>
+                            <li>+55 (45) 97954-3961</li>
+                            <br>
+                            <li>+55 (45) 94465-7501</li>
+                            <br>
+                            <li>+55 (45) 92589-2549</li>
+                            <br>
+                            <li>+55 (45) 95697-9734</li>
+                    </ul>
+                </div>
+
+                <div class="lista">
+                    <div style="margin-bottom: 2.737%;">
+                        <h1 style="color: rgb(255, 255, 255);">Redes Sociais</h1>
+
+
+
+                        <ul>
+                            <i class="bi bi-instagram" style="padding-right: 2%;"></i>
+                            <li>ShoesSportOfficial</li>
+                        </ul>
+
+                        <ul>
+                            <i class="bi bi-youtube" style="padding-right: 2%;"></i>
+                            <li>ShoesSportVideos</li>
+                        </ul>
+
+                        <ul>
+                            <i class="bi bi-facebook" style="padding-right: 2%;"></i>
+                            <li>ShoesSport</li>
+                        </ul>
+
+
+
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </footer>
+        <!-- Fim Footers -->
